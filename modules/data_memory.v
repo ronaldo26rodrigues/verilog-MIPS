c00755d6e000d23e389848656121775b69b1a989
Vicente Alencar Sampaio <vicentegalencar@gmail.com>
2022-09-20 22:34:21 -0300

    atualizacao data_mem e ULA

--- modules/data_memory.v
+++ modules/data_memory.v
@@ -1,51 +1,30 @@
+module data_memory (
+input wire [31:0] addr,          // Memory Address
+input wire [31:0] write_data,    // Memory Address Contents
+input wire memwrite, memread,
+input wire clk,                  // All synchronous elements, including memories, should have a clock signal
+output reg [31:0] read_data      // Output of Memory Address Contents
+);
 
- module data_memory  
- (  
- 
- 
- 
-      
-      input      [31:0]               	endereço,  
-		input      [31:0]               	write_data, 
-		input 	  [5:0]                 opcode,
-		
-		//input                        		clk,  
-      input                         	sinal_mem_write,  
-      input 									sinal_mem_read,  		
-		
-      output  reg[31:0]                read_data  
- );  
- 
- 
-	
-	reg [31:0] data_mem [255:0];
-	
-	initial begin
-		$readmemb("data.mem", data_mem);
-	end
-	
-	always @(endereço) begin
-		if(sinal_mem_write) begin
-			if(opcode == 6'h28) begin
-				data_mem[endereço][7:0] = write_data[7:0];
-			end
-			else if(opcode == 6'h29) begin
-				data_mem[endereço][15:0] = write_data[15:0];
-			end
-			else begin
-				data_mem[endereço] = write_data;
-			end
-			// write to file
-			$writememb("data.mem", data_mem);
-		end
-	end
-	
-	always @(endereço) begin
-		if(sinal_mem_read) begin
-			read_data = data_mem[endereço];
-		end
-	end
-	
-	
-	
+reg [31:0] MEMO[0:255];  // 256 words of 32-bit memory
+
+integer i;
+
+initial begin
+  read_data <= 0;
+  for (i = 0; i < 256; i = i + 1) begin
+    MEMO[i] = i;
+  end
+end
+
+always @(posedge clk) begin
+  if (memwrite == 1'b1) begin
+    MEMO[addr] <= write_data;
+  end
+
+  if (memread == 1'b1) begin
+    read_data <= MEMO[addr];
+  end
+end
+
 endmodule
--- modules/ula.v
+++ modules/ula.v
@@ -1,26 +1,49 @@
+//32 bit sayılarla Arithmetic işlemler yapan modül. 
+//Tüm instructionların yapması gerekenler burada halledildi. 
+module alu(
+          input		[3:0]	ctl,
+		input		[31:0]	a,
+		input		[31:0]	b,
+		output reg	[31:0]	saida,
+		output				zero);
+    );
+    reg [7:0] resultado;
+    wire [8:0] tmp;
+    assign saida = resultado; 
+    assign tmp = {1'b0,A} + {1'b0,B};
+    assign zero = tmp[8]; 
+    always @(*)
+    begin
+        case(ALU_Sel)
+        4'b0000: // Addition
+           resultado = A + B ; 
+        4'b0001: // Subtraction
+           resultado = A - B ;
+        4'b0010: // Multiplication
+           resultado = A * B;
+        4'b0011: // Division
+           resultado = A/B;
+        4'b0100: // Logical shift left
+           resultado = A<<1;
+         4'b0101: // Logical shift right
+           resultado = A>>1;
+          4'b1000: //  Logical and 
+           resultado = A & B;
+          4'b1001: //  Logical or
+           resultado = A | B;
+          4'b1010: //  Logical xor 
+           resultado = A ^ B;
+          4'b1011: //  Logical nor
+           resultado = ~(A | B);
+          4'b1100: // Logical nand 
+           resultado = ~(A & B);
+          4'b1101: // Logical xnor
+           resultado = ~(A ^ B);
+        endcase
+    end
 
-module ula(       
-      input          [31:0]     a,          
-      input          [31:0]     b,          
-      input          [3:0]     ula_control,      
-      output     reg     [31:0]     result,              
-      output zero  
-   );  
- always @(*)  
- begin   
-      case(ula_control)  
-      3'b000: result = a + b;  
-      3'b001: result = a - b;  
-      3'b010: result = a & b;  
-      3'b011: result = a | b; 
-      3'b100: begin if (a<b) result = 32'd1;  
-                     else result = 132'd0;  
-                     end  
-      default:result = a + b; 
-      endcase  
- end  
- assign zero = (result==32'd0) ? 1'b1: 1'b0;  
- endmodule  
- 
- 
- 
+endmodule
+
+
+
+
