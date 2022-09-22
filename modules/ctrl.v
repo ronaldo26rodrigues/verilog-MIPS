f03e1d3f2875ac56d78e78222f8f49f94e10c42d
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2022-09-22 00:01:14 -0300

    control+

--- modules/ctrl.v
+++ modules/ctrl.v
@@ -1,6 +1,6 @@
 module ctrl (
-    output RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;
-    output [1:0] ALUOp;
+    output RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, BNE, BEQ, JAL, JR, JMP;
+    output reg [1:0] ALUOp;
     input [31:0] instruction
     output [5:0] funct;
 );
@@ -37,28 +37,39 @@ module ctrl (
                 MemWrite = 1'b0;
                 Branch = 1'b0;
                 ALUOp = 2'b10;
+                BEQ = 1'b0;
+                BNE = 1'b0;
+                JMP = 1'b0;
+                JAL = 1'b0;
+                JR = 1'b0;
             end
 
 
         endcase
 
         assign RegDst = (instruction[31:26] == 6'b000000);
-        assign MemRead = (instr[31:26] == 6'b100011);
-    	assign MemtoReg = (instr[31:26] == 6'b100011);
-        assign MemWrite = (instr[31:26] == 6'b101011);
+        assign MemRead = (instruction[31:26] == 6'b100011);
+    	assign MemtoReg = (instruction[31:26] == 6'b100011);
+        assign MemWrite = (instruction[31:26] == 6'b101011);
+        assign BEQ = (instruction[31:26] == 6'b000100);
+        assign BNE = (instruction[31:26] == 6'b000101);
+        assign JMP = (instruction[31:27] == 6'b00001); 
+        assign JAL = (instruction[31:26] == 6'b000011);
+        assign JR = ((instruction[31:26] == 6'b000000) && (instruction[5:0] == 6'b001000));
 
-        assign ALUSrc = ((instr[31:26] == 6'b100011) || //lw
-                         (instr[31:26] == 6'b101011) || //sw
-                         (instr[31:26] == 6'b001101) || //ori
-                         (instr[31:26] == 6'b001000)    //addi
+
+        assign ALUSrc = ((instruction[31:26] == 6'b100011) || //lw
+                         (instruction[31:26] == 6'b101011) || //sw
+                         (instruction[31:26] == 6'b001101) || //ori
+                         (instruction[31:26] == 6'b001000)    //addi
                         );
 
-        assign RegWrite = ((instr[31:26] == 6'b100011) || //lw
-                           (instr[31:26] == 6'b001111) || //lui
-                           (instr[31:26] == 6'b001101) || //ori
-                           (instr[31:26] == 6'b001000) || //addi
-                           (instr[31:26] == 6'b000011) || //jal
-                           ((instr[31:26] == 6'b000000)) //R-type
+        assign RegWrite = ((instruction[31:26] == 6'b100011) || //lw
+                           (instruction[31:26] == 6'b001111) || //lui
+                           (instruction[31:26] == 6'b001101) || //ori
+                           (instruction[31:26] == 6'b001000) || //addi
+                           (instruction[31:26] == 6'b000011) || //jal
+                           ((instruction[31:26] == 6'b000000)) //R-type
                           );
 
     end
